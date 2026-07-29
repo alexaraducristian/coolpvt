793e7501b86a6946bf127edc88d8072ed47f0c4b
Radu Alexa <radu.alexa@aumovio.com>
2026-07-29 13:26:46 +0300

    Update stylesheet to fit main site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwindvite from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://www.alexavolt.ro',
   base: '/coolpvt',
+  trailingSlash: 'always',
+  vite: {
+    plugins: [tailwindvite()],
+  },
 });
