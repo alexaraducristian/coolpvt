401856e062dc95e2e94829f9bf670d5729afc0d3
Radu Alexa <radu.alexa@aumovio.com>
2026-07-29 16:10:58 +0300

    Update coolpvt dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindvite from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://www.alexavolt.ro',
-  base: '/coolpvt',
+  site: 'https://coolpvt.alexavolt.ro',
+  base: '/',
   trailingSlash: 'always',
   vite: {
     plugins: [tailwindvite()],
